91fdd9628c88357b9d59ab4bd05a771c59aa1656
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2020-09-26 13:11:03 +0500

    working on articles section

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,21 +4,12 @@ import PatuaOne from 'typeface-roboto';
 const TagStyle = styled.div`
     padding:5px 10px;
     margin:0px 10px;
-    border-radius:15px;
-    font-size:0.9em;
+    
     font-family:'Roboto';
-    background-color:#a5f9ff;
-    &:hover{
-        background-color:#2196f3;
-        .tag__link{
-  
-        color:white;
- 
-    }
-    }
     .tag__link{
         text-decoration:none;
         color:#0069c0;
+        text-transform:'uppercase';
  
     }
     `;
